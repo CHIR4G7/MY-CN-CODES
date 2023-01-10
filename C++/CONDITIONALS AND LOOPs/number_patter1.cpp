1dc8a0a7031e04f2ff1bb418dafca3b032a95bf6
CHIR4G7 <93506828+CHIR4G7@users.noreply.github.com>
2023-01-10 12:37:50 +0530

    Create number_patter1.cpp

---
+++ C++/CONDITIONALS AND LOOPs/number_patter1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    int i = 1;
+    while(i<=N)
+    {
+        int j = 1;
+        int var = i;
+        while(j<=i)
+        {
+           
+           cout << var;
+           var = var + 1;
+           j = j + 1;
+
+        }
+        cout << endl;
+        i = i + 1;
+    }
+}
